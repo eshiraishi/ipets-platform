141315dc353c0faed28c98b178b3434e51e11fca
Enzo Shiraishi <enzo.shiraishi@gmail.com>
2023-04-19 13:07:23 -0300

    chore: update CORS

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="iPets",
-    description="Especificacao dos contratos disponibilizados pela plataforma do aplicativo iPets, um\naplicativo para contratacao de servicos para pets.\nParte do projeto do grupo 21 na disciplina de Engenharia de Software, ministrada em\n2023.1 na UFABC.",
+    description="Especificacao dos contratos disponibilizados pela plataforma do aplicativo iPets, um\naplicativo para contratacao de servicos para pets.\nParte do projeto do grupo 21 na disciplina de Engenharia de Software, ministrada em\n2023.1 na UFABC.",  # noqa: E501
     version="0.1.0",
     servers=[{"url": "http://ipets.azurewebsites.net/"}],
     dependencies=[Depends(get_database)],
@@ -25,6 +25,7 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+    expose_headers=["*"],
 )
 
 app.include_router(provider.router)
