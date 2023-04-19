b57a2e87dc380143632abbd988a0030f39d73ce5
Fernando Cezar Resende Lopes <fernandocrlopes1@gmail.com>
2023-04-19 14:41:12 -0300

    ci: update CORS issue with swagger 'try it out' button

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ app = FastAPI(
     title="iPets",
     description="Especificacao dos contratos disponibilizados pela plataforma do aplicativo iPets, um\naplicativo para contratacao de servicos para pets.\nParte do projeto do grupo 21 na disciplina de Engenharia de Software, ministrada em\n2023.1 na UFABC.",  # noqa: E501
     version="0.1.0",
-    servers=[{"url": "http://ipets.azurewebsites.net/"},{"url": "http://127.0.0.1/"}],
+    servers=[{"url": "https://ipets.azurewebsites.net/"}],
     dependencies=[Depends(get_database)],
 )
 
